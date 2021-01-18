4c807cc77b0b5d82cdf0dc6e110508c59e7da40b
DeanWalsh <deanochambers@gmail.com>
2021-01-18 15:02:55 +0000

    sidebar mouseleave fix

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,21 +1,13 @@
-/* Set the width of the sidebar to 250px (show it) */
 function openNav1() {
-    document.getElementById("mySidepanel1").style.width = "400px";
-    document.getElementById("mySidepanel2").style.width = "0";
-  }
-  
-  /* Set the width of the sidebar to 0 (hide it) */
-  function closeNav1() {
-    document.getElementById("mySidepanel1").style.width = "0";
-
-  }
+  document.getElementById("mySidepanel1").style.width = "400px";
+}
+function closeNav1() {
+  document.getElementById("mySidepanel1").style.width = "0"; 
+}
 function openNav2() {
-    document.getElementById("mySidepanel2").style.width = "400px";
-    document.getElementById("mySidepanel1").style.width = "0";
-  }
-  
-  /* Set the width of the sidebar to 0 (hide it) */
-  function closeNav2() {
-    document.getElementById("mySidepanel2").style.width = "0";
-  }
+  document.getElementById("mySidepanel2").style.width = "400px";
+}
+function closeNav2() {
+  document.getElementById("mySidepanel2").style.width = "0";
+}
   
